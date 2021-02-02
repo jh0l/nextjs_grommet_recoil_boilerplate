3e9acdbd562647ac6a78372812ca5a1bede072cc
jh0l <me@jh0l.com>
2021-02-03 10:22:53 +1100

    add ts types index

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+declare module '*.gif';
+
+// individual icon components for tree shaking
+
+declare module 'grommet-icons/icons/FormClose';
