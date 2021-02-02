b2bd9cde873b66b77334410dc551e6cac4f2d607
jh0l <me@jh0l.com>
2021-02-03 10:56:19 +1100

    add Nextjs Head content component to _app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,25 @@ import {theme} from '../theme';
 import {NavBar} from '../components/NavBar';
 import {NotificationLayer} from '../components/Notifications';
 
+import Head from 'next/head';
+
+const HeadContent = ({title}: {title: string}) => {
+    return (
+        <Head>
+            <title>{title}</title>
+            <meta charSet="utf-8" />
+            <meta
+                name="viewport"
+                content="initial-scale=1.0, width=device-width"
+            />
+        </Head>
+    );
+};
+
 export default function App({Component, pageProps}: AppProps) {
     return (
         <RecoilRoot>
+            <HeadContent title="[AppName]" />
             <Grommet theme={theme}>
                 <NavBar />
                 <Component {...pageProps} />
